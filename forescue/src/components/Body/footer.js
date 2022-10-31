6432a4cc91fed11c67d9528cb6b0b4144005b19b
teddyrugged <jakpaetuk@gmail.com>
2022-10-31 10:51:57 +0100

    fixed automated year

--- forescue/src/components/Body/footer.js
+++ forescue/src/components/Body/footer.js
@@ -5,41 +5,20 @@ import "./dashboard"
 const Footer = () => {
   return (
     <div>
-        {/* <h1>Get in touch with us</h1>
-        <form >
-            <h2>Send us a message</h2>
-            <input type= "text" id="text" name="firstname"/>
-            <input type= "text" id="text" name="lastname"/>
-            <label for="email">Your Email</label>
-            <input type="email" id="email" name="email"/>
-            <input type= "text" id="text" name="message"/>
 
-            <input type="submit" value="Submit"/>
-        </form> */}
         <div className='wid--foot'>
-            {/* <div className='middle--man'>
-                <h2>FORESCUE</h2>
-                <h3>subscribe to get alert</h3>
-                <form className='d--forms'>
-                    <input  className='input--1'   type= "text" id="text" name="enter your email address"/>
-                    <input className='input--2' type="submit" value="Subsribe"/>
-                </form>
-                <div class="hl"></div>
-            </div> */}
-
-
-
+            <br></br>
             <footer>
-                <p>© 2022 All Rights Reserved by Forescue</p>
+                <p>© {new Date().getFullYear()} All Rights Reserved by Forescue</p>
                 <div className='biz'>
-                    <a href = "/"> Privacy Policy</a>
-                    <a href = "/"> Term of use</a>
-                    <a href = "/"> How it works</a>
-                    <a href = "/"> Tutorial</a>
-                    <a href = "/"> Site Map</a>
+                    <ul>
+                        <li><a href = "/"> Privacy Policy</a></li>
+                        <li><a href = "/"> Term of use</a></li>
+                        <li><a href = "/"> How it works</a></li>
+                        <li><a href = "/"> Tutorial</a></li>
+                        <li><a href = "/"> Site Map</a></li>
+                    </ul>
                 </div>
-
-
             </footer>
 
         </div>
